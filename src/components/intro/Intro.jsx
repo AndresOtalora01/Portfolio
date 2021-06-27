e714411dfabd176e6a53907ba5ee44b3f6550a6f
andre <andresro0198@gmail.com>
2021-06-27 20:43:34 +0200

    text typing animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,36 @@
 import "./intro.scss"
-
+import {init} from "ityped"
+import { useEffect, useRef } from "react"
 export default function Intro(){
+
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current,
+             { showCursor: true,
+                backDelay: 1000,
+                backSpeed: 60,
+             strings: ['Android','Java', 'Kotlin', 'MySQL', 'MongoDB and more.' ] })
+    },[])
+
     return (
         <div className="intro" id="intro">
-
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/boy.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I'm</h2>
+                    <h1>Andrés Otálora</h1>
+                    <h3>Software Developer using <span ref={textRef}></span>
+                    </h3> 
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
