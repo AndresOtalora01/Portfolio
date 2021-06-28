8729c17999b2fbb7de540f73e21b2bdc5ed06735
andre <andresro0198@gmail.com>
2021-06-28 20:58:57 +0200

    portfolio list

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,13 +1,29 @@
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss"
+import React, { useEffect, useState } from "react";
 
-export default function portfolio(){
+export default function Portfolio(){
+
+    const [selected, setSelected] = useState("featured");
+
+    const list = [
+        {
+            id: "featured", title: "Featured"
+        }, 
+        {
+            id: "mobile", title: "Mobile apps"
+        }, 
+        {
+            id:"developing", title: "Developing"
+        }
+    ];
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <ul>
-                <li className="active">Featured</li>
-                <li>Mobile apps</li>
-                <li>Developing</li>
+                {list.map(item=>(
+                    <PortfolioList title={item.title} active = {selected === item.id} setSelected={setSelected} id = {item.id}/>
+                ))}
             </ul>
             <div className="container">
                 <div className="item">
