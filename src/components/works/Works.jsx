ee2929da640a4f5910ba35b8c01c5f88165e7a5f
andre <andresro0198@gmail.com>
2021-06-30 01:45:25 +0200

    works slider implemented

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,6 +1,8 @@
 import "./works.scss"
-
+import React, { useEffect, useState } from "react";
 export default function Works(){
+
+    const [currentSlide,setCurrentSlide] =useState(0);
 
     const data = [
         {
@@ -8,29 +10,40 @@ export default function Works(){
             icon: "./assets/hdpi.png",
             title: "Groupz",
             desc: "Es una aplicación que te permite conocer gente nueva a través de eventos creados por ti o por la comunidad. Está desarrollada nativamente en Android utilizando kotlin y como base de datos Firestore.",
-            img: "./assets/combine_images.png"
+            img: "./assets/combine_images.png",
+            link: "https://play.google.com/store/apps/details?id=cat.copernic.groupz"
         },
         {
             id: 2,
             icon: "./assets/ruedawhite.png",
             title: "Andruino",
             desc: "Con Andruino podemos controlar un dron terrestre fabricado con un dispositivo ESP32 a través de la aplicación nativa de android con Kotlin y BLE (bluetooth low energy). Este dispositivo envía información como su ubicación en tiempo real a la base de datos entre otros.",
-            img: "./assets/andruino2.jpg"
+            img: "./assets/andruino2.jpg",
+            link: "https://github.com/AndresOtalora01/andruino"
         },
         {
             id:3,
             icon: "./assets/secure.png",
             title:"Hash Generator",
             desc: "Utilizando hash generator podemos transformar texto en un hash utilizando diferentes algoritmos de encriptación como MD5, SHA-256 y más. Se ha desarrollado para Android utilizando kotlin.",
-            img:"./assets/hash2.jpg"
+            img:"./assets/hash2.jpg",
+            link:"https://github.com/AndresOtalora01/HashGenerator"
         }
     ];
+
+    const handleClick = (way) => {
+        way === "left"
+          ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+          : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+      };
+
+
 
 
     return (
         <div className="works" id="works">
 
-        <div className="slider">
+        <div className="slider" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
             {data.map((d)=>(
             <div className="container">
                 <div className="item">
@@ -41,7 +54,7 @@ export default function Works(){
                             </div>
                             <h2>{d.title}</h2>
                             <p>{d.desc}</p>
-                            <span>Projects</span>
+                            <a href={d.link}>See more</a>
                         </div>
                     </div>
                     <div className="right">
@@ -51,8 +64,8 @@ export default function Works(){
                 
             </div>))}
         </div>
-        <img src="assets/down.png" className="arrow left" alt="" />
-        <img src="assets/down.png" className="arrow right" alt="" />
+        <img src="assets/down.png" className="arrow left" alt="" onClick={() => handleClick("left")}/>
+        <img src="assets/down.png" className="arrow right" alt="" onClick={() => handleClick()} />
         </div>
     );
 }
